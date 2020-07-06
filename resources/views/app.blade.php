cf7d1cd4a95f5263eccaa8359a3fbf81a298c79c
Kok Wai <kwai8891@gmail.com>
2020-07-07 01:09:35 +0800

    Remove unused code

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,8 +7,6 @@
   <title>Faceper</title>
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-  <!-- <link rel="stylesheet" href="{{ asset('css/material/material-dashboard.css') }}" type="text/css"> -->
-
 </head>
 
 <body class="">
