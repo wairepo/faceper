9abea60fa28659fee02113c1a17f4ee3d7c19462
Kok Wai <kwai8891@gmail.com>
2020-07-12 22:53:11 +0800

    Give favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,10 +5,11 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>Faceper</title>
+  <link rel="icon" href="{{ asset('img/favicon.png') }}" type="image/png">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
   <link rel="stylesheet" href="{{ asset('css/material/sidebar.css') }}" type="text/css">
-
+ 
 </head>
 <style type="text/css">
 	.selector-for-some-widget {
